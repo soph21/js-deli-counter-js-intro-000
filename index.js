8d57e422aea1d17ff3ca0bacb88a901ab3a6bbfc
Sophie Shin <sophie@alloy.co>
2021-03-18 15:48:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ katzDeli.push (name)
   return `Welcome, ${name}. You are number ${katzDeli.indexOf(name)+1} in line.`
 }
 
-function nowServing (katzDeliLine) {
-  if katzDeliLine.length === 0 {
+function nowServing (deliLine) {
+  if deliLine.length === 0 {
     return "There is nobody waiting to be served"
   }
 }
