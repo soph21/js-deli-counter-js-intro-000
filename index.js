8eae439a11b2ced72aef025699bd697f134723c2
Sophie Shin <sophie@alloy.co>
2021-03-18 14:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeli, name) {
 katzDeli.push (name)
-  return `Welcome, ${name}. You are number ${katzDeli.indexOf(name)} in line.`
+  return `Welcome, ${name}. You are number (${katzDeli.indexOf(name)}+1) in line.`
 }
